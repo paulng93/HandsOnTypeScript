7a58941f01ffc8153ca8c3800ff667d13adcf4c2
paulng93 <paulng93@gmail.com>
2024-03-08 19:04:29 -0800

    working copy

--- super-forum-client/src/store/AppState.ts
+++ super-forum-client/src/store/AppState.ts
@@ -1,6 +1,7 @@
 import { combineReducers } from "redux";
 //import { UserProfileReducer } from "./user/Reducer";
 
+// default root reducer that combines all future reducers
 export const rootReducer = combineReducers({
   //user: UserProfileReducer,
 });
--- super-forum-client/src/store/configureStore.ts
+++ super-forum-client/src/store/configureStore.ts
@@ -1,8 +1,15 @@
-import { createStore } from "redux";
+import { configureStore } from '@reduxjs/toolkit'
 import { rootReducer } from "./AppState";
 
+// friendly abstraction over Redux CreateStore() function 
+export default configureStore({
+  reducer: rootReducer,
+});
+
+/*
 const configureStore = () => {
   return createStore(rootReducer, {});
 };
 
 export default configureStore;
+*/
